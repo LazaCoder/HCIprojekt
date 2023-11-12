a581989078442a3c0f9c06322f291a578be5d8b0
LazaCoder <il53465@fer.hr>
2023-11-12 12:16:09 +0100

    Add fetched profile from cms

--- semester-project/nextjs-projekt/pages/profile/[id].js
+++ semester-project/nextjs-projekt/pages/profile/[id].js
@@ -1,24 +1,38 @@
 import { createClient } from 'contentful';
-import { useRouter } from 'next/router';
+import styles from './Profile.module.css'; // Assuming you have a CSS module for styling
 
 export default function Profile({ userProfile }) {
-  const router = useRouter();
-  const { id } = router.query;
-
-  if (router.isFallback) {
-    return <div>Loading...</div>;
+  if (!userProfile) {
+    return <div>Loading...</div>; // This will show when the page is being statically generated
   }
 
   return (
-    <div>
-      <h1>{userProfile.name}</h1>
-      <p>{userProfile.bio}</p>
+    <div className={styles.profileContainer}>
+      <h1 className={styles.profileTitle}>{userProfile.name}</h1>
+      <p className={styles.profileBio}>{userProfile.bio}</p>
       {/* Display other profile fields as needed */}
     </div>
   );
 }
 
-export async function getServerSideProps({ params }) {
+export async function getStaticPaths() {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+  });
+
+  const entries = await client.getEntries({ content_type: 'userProfile' });
+  const paths = entries.items.map((item) => ({
+    params: { id: item.fields.userId.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: 'blocking',
+  };
+}
+
+export async function getStaticProps({ params }) {
   const client = createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
@@ -26,18 +40,17 @@ export async function getServerSideProps({ params }) {
 
   const response = await client.getEntries({
     content_type: 'userProfile',
-    'fields.userId': params.userId,
+    'fields.userId': params.id,
   });
 
   if (!response.items.length) {
-    return {
-      notFound: true,
-    };
+    return { notFound: true };
   }
 
   return {
     props: {
       userProfile: response.items[0].fields,
     },
+    revalidate: 60, // ISR: Revalidate at most once per minute
   };
 }
