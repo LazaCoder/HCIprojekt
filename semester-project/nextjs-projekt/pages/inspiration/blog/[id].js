2e938bec034e55ced68d82a473a68a0c1b5f1140
LazaCoder <il53465@fer.hr>
2024-02-13 16:26:39 +0100

    design

--- semester-project/nextjs-projekt/pages/inspiration/blog/[id].js
+++ semester-project/nextjs-projekt/pages/inspiration/blog/[id].js
@@ -7,6 +7,7 @@ import { useEffect,useState } from 'react';
 import client from '../../api/contentfulClient';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 
 
 // Dummy data for the blog post
